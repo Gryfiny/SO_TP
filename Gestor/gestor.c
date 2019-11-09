df42c2ea7a37a0446b6c5b0972d4fcdcd13426c5
BRodrigues98 <bruno.m.v.rodrigues1998@gmail.com>
2019-11-09 18:30:37 +0000

    Gestor + cliente

---
+++ Gestor/gestor.c
@@ -0,0 +1,62 @@
+#include "gestor.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+    //Variaveis de ambiente:   	 - maxmsg- máximo de mensagens
+    //				                         - maxnot - maximo de palavras proibidas
+    //                         				 - wordsnot - nome do ficheiro onde estão as palavras proibidas
+
+    int maxmsg, maxnot;
+    char *wordsnot;
+    int flagVA = getVarsAmb(&maxmsg, &maxnot, wordsnot);
+
+    printf("MAXMSG = %d\nMAXNOT = %d\nWORDSNOT = %s\n", maxmsg, maxnot, wordsnot);
+    if(flagVA){
+        exit(3);
+	}
+	
+	printf("Gestor:\n");
+		
+	char *comando;
+	while(1){
+		
+	}
+
+    exit(0);
+}
+
+int getVarsAmb(int *maxmsg, int *maxnot, char *wordsnot){
+    char *tempmm = getenv("MAXMSG");
+
+
+    if(tempmm != NULL)
+        *maxmsg = atoi(tempmm);
+    else{
+        fprintf(stderr, "[ERRO] MAXMSG nao encontrado.\n");
+        exit(3);
+    }
+
+    tempmm = getenv("MAXNOT");
+    if(tempmm != NULL)
+        *maxnot = atoi(tempmm);
+    else{
+        fprintf(stderr, "[ERRO] MAXNOT nao encontrado.\n");
+        exit(3);
+    }
+
+
+    char *wordsnotTemp = getenv("WORDSNOT");
+    if(wordsnotTemp == NULL){
+        fprintf(stderr, "[ERRO] WORDSNOT nao encontrado.\n");
+        exit(3);
+    }
+
+    strcpy(wordsnot, wordsnotTemp);
+    return 0;
+}
+
---
+++ Gestor/gestor.h
@@ -0,0 +1,14 @@
+#ifndef GESTOR_H_INCLUDED
+#define GESTOR_H_INCLUDED
+
+//typedef struct gestor ge;
+//struct gestor{
+//    int nClientes;
+//
+//};
+
+int main(int argc, char *argv[]);
+int getVarsAmb(int *maxmsg, int *maxnot, char *wordsnot);
+int comando();
+
+#endif // GESTOR_H_INCLUDED
